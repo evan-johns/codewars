79b5815b418a595e94032c9507c1d0ce527711f3
Evan Johns <evandjohns@gmail.com>
2011-05-08 14:00:00 +0100

    8kyu practice return negative

---
+++ js/solution008.js
@@ -0,0 +1,15 @@
+//8kyu Multiply
+
+// This code does not execute properly. Try to figure out why.
+
+// function multiply(a, b){
+//     a * b
+//   }
+
+
+
+function multiply(a,b){
+    return Number(a*b)
+   }
+
+//added return to line 3 and wrapped a * b in Number()
