0659340e6906e107439da8f289653046c3e18040
evan-johns <evandjohns@gmail.com>
2022-06-08 10:27:01 -0400

    even or odd 8kyu practice

---
+++ js/solution013.js
@@ -0,0 +1,10 @@
+// Return "Even" or "Odd" accordingly for any given integer, positive or negative.
+
+function even_or_odd(number) {
+    if(number % 2 === 0) {
+      return "Even" 
+    } 
+    else {
+      return "Odd"
+    } 
+  }
